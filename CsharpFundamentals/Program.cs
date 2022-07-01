7833135b78bac265d3c8a76f96fc6b9f618812ef
madiha gul <mmadihagull@gmail.com>
2022-07-01 18:40:14 +0200

    Add project files.

---
+++ CsharpFundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyUtilities;
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("\n Where should we go in May");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelcius(65), 73);
+            WeatherUtilities.Report("Danver", WeatherUtilities.FahrenheitToCelcius(77), 55);
+            WeatherUtilities.Report("Bologna",23,65);
+        }
+    }
+}
---
+++ CsharpFundamentals/WeatherUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyUtilities
+{
+    class WeatherUtilities
+    {
+        public static float FahrenheitToCelcius(float tempFahrenheit)
+        {
+            return (tempFahrenheit - 32) / 1.8f;
+        }
+        static float CelciusToFahrenheit(float tempCelcius)
+        {
+            return tempCelcius *1.8f+32;
+        }
+
+        // Higher index low comfort
+        static float ComfortIndex(float tempFahrenheit, float humidityPercent)
+        {
+            //Not very reliable formula
+            return (tempFahrenheit+ humidityPercent)/4;
+        }
+
+        public static void Report(String location,float tempCelcius, float humidityPercent)
+        {
+            var tempFahrenheit = CelciusToFahrenheit(tempCelcius);
+            Console.WriteLine($"Comfort Index for {location}:{ComfortIndex(tempFahrenheit,humidityPercent)} ");
+        }
+
+    }
+
+}
